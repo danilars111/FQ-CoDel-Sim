f35c96ec475f83d3d8de942a991b2d7be5ae53ad
martwahl05 <martin_p95@hotmail.se>
2018-03-06 16:20:02 +0100

    first

--- basic.py
+++ basic.py
@@ -1,41 +1,90 @@
 import salabim as sim
 
+
 class queue(sim.Component):
-    def process(self):
+    def setup(self):
         self.queue = sim.Queue('queue')
-        yield self.passivate()
+        self.credits = 0
+        self.new = True
 
-class move(sim.Component):
+    def move(self, source, destination):
+        self.leave(source)
+        self.enter(destination)
+    
+    
+    def push(self, component):
+        component.enter(self.queue)
+
+
+class scheduler(sim.Component):
     def process(self):
-        self.queue = passiveQueues.pop()
-        self.queue.enter(newQueues)
-        yield self.passivate()
+        old = False
+        counter = 0
 
-class addToQueue(sim.Component):
-    def process(self):
-        self.queue = newQueues[0]
-        data.enter(self.queue.queue)
-        yield self.passivate
+        while True:
+            if newQueues and not old:
+                queue = newQueues[0].queue
+                newQueues[0].move(newQueues, oldQueues)
+            
+            else:
+                queue = oldQueues[0].queue
+                
+                old = True
+                counter += 1
+                
+                if queue:
+                    oldQueues[0].move(oldQueues, oldQueues)
 
-class data(sim.Component):
-    def nothing(self):
-        self.hold(1)
+                else:
+                    oldQueues[0].move[oldQueues, passiveQueues]
+
+
+
+            if counter is len(oldQueues):
+                old = False
+                counter = 0
+
+            if oldQueues[0] or newQueues[0]:
+                clerk.activate(queue=queue)
+
+            yield self.passivate()
+
+
+class clerk(sim.Component):
+    def process(self, queue):
+        while True:
+            queue.pop()
+            scheduler.activate()
+            yield self.passivate()
 
 
 env = sim.Environment(trace=True)
 
-interArrivalTime = sim.Uniform(5,15).sample()
 
 passiveQueues = sim.Queue('passiveQueues')
 
-for _ in range(5):
-    queue().enter(passiveQueues)
+#for _ in range(5):
+ #   queue().enter(passiveQueues)
+
 
 newQueues = sim.Queue('newQueues')
-oldQueues = sim.Queue('newQueues')
+oldQueues = sim.Queue('oldQueues')
 
-move()
-data = data()
-addToQueue()
+for y in range(5):
+    queue().enter(newQueues)
+
+    for _ in range(5):
+        box = sim.Component('box')
+        box.enter(newQueues[y].queue) 
+
+for y in range(5):
+    queue().enter(oldQueues)
+
+    for _ in range(5):
+        box = sim.Component('box')
+        box.enter(oldQueues[y].queue) 
+
+scheduler = scheduler()
+clerk = clerk(process=None)
 
 env.run(till=100)
