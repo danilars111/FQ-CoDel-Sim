6b0dbd1258c623700d7733a697eabdda9b1a816f
martwahl05 <martin_p95@hotmail.se>
2018-03-07 12:38:59 +0100

    scheduler

--- basic.py
+++ basic.py
@@ -1,11 +1,46 @@
 import salabim as sim
 
+FLOWS = 2
+
+class flowGenerator(sim.Component):
+    def process(self,fid):
+        while True:
+            flow(name='flow' + str(fid), fid=fid)
+            yield self.hold(sim.Uniform(5,15).sample())
+
+
+class flow(sim.Component):
+    def setup(self, fid):
+        self.fid = fid
+        self.packetSize = 1500 * 8
+
+    def process(self):
+        for i in range(len(newQueues)):
+            if newQueues[i].qid is self.fid:
+                newQueues[i].push(self)
+                yield self.cancel()
+
+        for j in range(len(oldQueues)):
+            if oldQueues[j].qid is self.fid:
+                oldQueues[j].push(self)
+                yield self.cancel()
+
+        passiveQueues[0].push(self)
+        passiveQueues[0].qid = self.fid
+        passiveQueues[0].move(passiveQueues, newQueues)
+
+        if scheduler.ispassive():
+            scheduler.activate()
+        
+        yield self.cancel()
+
 
 class queue(sim.Component):
     def setup(self):
-        self.queue = sim.Queue('queue')
+        self.queue = sim.Queue()
         self.credits = 0
         self.new = True
+        self.qid = -1
 
     def move(self, source, destination):
         self.leave(source)
@@ -18,73 +53,66 @@ class queue(sim.Component):
 
 class scheduler(sim.Component):
     def process(self):
-        old = False
+        old = False 
         counter = 0
 
         while True:
             if newQueues and not old:
                 queue = newQueues[0].queue
+                clerk.activate(queue=queue)
+                yield self.passivate()
                 newQueues[0].move(newQueues, oldQueues)
-            
-            else:
+                 
+            elif oldQueues:
                 queue = oldQueues[0].queue
-                
+                 
                 old = True
                 counter += 1
-                
                 if queue:
+                    clerk.activate(queue=queue)
+                    yield self.passivate()
                     oldQueues[0].move(oldQueues, oldQueues)
-
                 else:
-                    oldQueues[0].move[oldQueues, passiveQueues]
+                    oldQueues[0].move(oldQueues, passiveQueues)
+            
+            elif not self.ispassive():
+               yield self.passivate()
 
-
-
-            if counter is len(oldQueues):
+            print(old)
+            if counter is len(oldQueues) and old:
+                print("counter reset")
                 old = False
                 counter = 0
 
-            if oldQueues[0] or newQueues[0]:
-                clerk.activate(queue=queue)
 
-            yield self.passivate()
 
 
 class clerk(sim.Component):
     def process(self, queue):
         while True:
             queue.pop()
+            yield self.hold(10)
             scheduler.activate()
-            yield self.passivate()
+            yield self.cancel()
 
 
 env = sim.Environment(trace=True)
 
+scheduler = scheduler()
+
+for i in range(FLOWS):
+    flowGenerator(fid=i)
 
 passiveQueues = sim.Queue('passiveQueues')
 
-#for _ in range(5):
- #   queue().enter(passiveQueues)
+for _ in range(FLOWS):
+    queue().enter(passiveQueues)
 
 
 newQueues = sim.Queue('newQueues')
 oldQueues = sim.Queue('oldQueues')
 
-for y in range(5):
-    queue().enter(newQueues)
 
-    for _ in range(5):
-        box = sim.Component('box')
-        box.enter(newQueues[y].queue) 
-
-for y in range(5):
-    queue().enter(oldQueues)
-
-    for _ in range(5):
-        box = sim.Component('box')
-        box.enter(oldQueues[y].queue) 
-
-scheduler = scheduler()
 clerk = clerk(process=None)
 
 env.run(till=100)
