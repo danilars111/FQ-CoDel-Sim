3588863999094986ebc7bd9406082291fd1a03f9
martwahl05 <martin_p95@hotmail.se>
2018-04-05 12:56:11 +0200

    refactoring and minor bugfixes

--- FQCoDelSim.py
+++ FQCoDelSim.py
@@ -47,15 +47,75 @@ MSS = 1500.0 * 8.0
 QUANTUM = MSS + (18.0 * 8.0)
 old = False
 
-class genFlow(sim.Component):
+while True:
+    BANDWIDTH = input("Enter bandwidth (Mbps): ") * pow(10,6)
+    if BANDWIDTH > 0:
+        break
+    print "invalid input, try again!"
+
+while True:
+    SPARSEFLOWS = input("Enter number of sparseflows: ")
+    if SPARSEFLOWS > 0:
+       break
+    print "invalid input, try again!"
+
+while True:
+    SPARSESIZE = input("Enter sparseflow packetsize(Byte): ") * 8.0
+    if SPARSESIZE > 0 or SPARSESIZE == -1*8:
+        break
+    print "invalid input, try again!"
+while True:
+    BULKFLOWS = input("Enter number of bulkflows: ")
+    if BULKFLOWS >= 0:
+        break
+    print "invalid input, try again!"
+
+while True:
+    INTERARRIVALTIME = input("Enter interarrival time(ms): ") * pow(10,-3)
+    if INTERARRIVALTIME > 0 or INTERARRIVALTIME == -1 * pow(10,-3):
+        break
+    print "invalid input, try again!"
+
+while True:
+    INTERARRIVALMULTIPLIER = input("Enter interarrivaltime multiplier ")
+    if INTERARRIVALMULTIPLIER > 0:
+        break
+    print "invalid input, try again!"
+
+while True:
+    RUNTIME = input("Enter simulated runtime: ")
+    if RUNTIME > 0:
+        break
+    print "invalid input, try again!"
+
+while True:
+    DETAILEDOUTPUT = input("Detailed output?: ")
+    if DETAILEDOUTPUT == 0 or DETAILEDOUTPUT == 1:
+        break
+    print "invalid input, try again!"
+
+while True:
+    TRACE = input("Trace?: ")
+    if TRACE == 0 or TRACE == 1:
+        break
+    print "invalid input, try again!"
+
+
+class flowGenerator(sim.Component):
     def process(self, waitTime):
+
+        for i in range(1,2*BULKFLOWS,2):
+            bulkPacketGenerator(pid=i,interTime=((QUANTUM/BANDWIDTH)-(1.0/BANDWIDTH)),distribution="uniform")
+        
+        yield self.hold((BULKFLOWS*QUANTUM)/BANDWIDTH)
+
         for i in range(0,2*SPARSEFLOWS,2):               
-            sparseFlowGenerator(fid=i,interTime=time*INTERARRIVALMULTIPLIER, distribution="uniform")
+            sparsePacketGenerator(pid=i,interTime=time*INTERARRIVALMULTIPLIER, distribution="uniform")
             yield self.hold(sim.Exponential(waitTime/SPARSEFLOWS).sample())
 
 
-class sparseFlowGenerator(sim.Component):
-    def process(self,fid, interTime, distribution="uniform"):
+class sparsePacketGenerator(sim.Component):
+    def process(self,pid, interTime, distribution="uniform"):
         spSize = 0
         
         if int(SPARSESIZE) == -1 * 8:
@@ -65,12 +125,12 @@ class sparseFlowGenerator(sim.Component):
 
         if distribution is "uniform":
             while True:
-                flow(name='sflow-' + str(fid), fid=fid, size = spSize )
+                packet(name='spacket-' + str(pid), pid=pid, size = spSize )
                 yield self.hold(interTime)
 
         elif distribution is "exponential":
             while True:
-                flow(name='sflow-' + str(fid), fid=fid, size = spSize)
+                packet(name='spacket-' + str(pid), pid=pid, size = spSize)
                 yield self.hold(sim.Exponential(interTime).sample())
         
         else:
@@ -78,17 +138,17 @@ class sparseFlowGenerator(sim.Component):
             yield self.cancel
 
 
-class bulkFlowGenerator(sim.Component):
-    def process(self,fid, interTime, distribution="uniform"):
+class bulkPacketGenerator(sim.Component):
+    def process(self,pid, interTime, distribution="uniform"):
        
         if distribution is "uniform":
             while True:
-                flow(name='bflow-' + str(fid), fid=fid, size=QUANTUM)
+                packet(name='bpacket-' + str(pid), pid=pid, size=QUANTUM)
                 yield self.hold(interTime)
 
         elif distribution is "exponential":
             while True:
-                flow(name='bflow-' + str(fid), fid=fid, size=QUANTUM)
+                packet(name='bpacket-' + str(pid), pid=pid, size=QUANTUM)
                 yield self.hold(sim.Exponential(interTime).sample())
         
         else:
@@ -96,9 +156,9 @@ class bulkFlowGenerator(sim.Component):
             yield self.cancel
 
 
-class flow(sim.Component):
-    def setup(self, fid, size):
-        self.fid = fid
+class packet(sim.Component):
+    def setup(self, pid, size):
+        self.pid = pid
         self.packetSize = size - (size % 8)
         self.timeOfArrival = env.now()
         self.timeOfRemoval = 0.0
@@ -106,19 +166,19 @@ class flow(sim.Component):
     def process(self):
         global old 
         for i in range(len(newQueues)):
-            if newQueues[i].qid is self.fid:
+            if newQueues[i].qid is self.pid:
                 newQueues[i].push(self)
                 newQueues[i].packetCounter += 1
                 yield self.cancel()
 
         for j in range(len(oldQueues)):
-            if oldQueues[j].qid is self.fid:
+            if oldQueues[j].qid is self.pid:
                 oldQueues[j].push(self)
                 oldQueues[j].packetCounter += 1
                 yield self.cancel()
 
         for k in range(len(passiveQueues)):
-            if passiveQueues[k].qid is self.fid:
+            if passiveQueues[k].qid is self.pid:
                 passiveQueues[k].push(self)
                 passiveQueues[k].packetCounter += 1
                 passiveQueues[k].activateCounter += 1
@@ -132,7 +192,7 @@ class flow(sim.Component):
         for l in range(len(passiveQueues)):
             if passiveQueues[l].qid is UNCLAIMED:
                 passiveQueues[l].push(self)
-                passiveQueues[l].qid = self.fid
+                passiveQueues[l].qid = self.pid
                 passiveQueues[l].move(passiveQueues, newQueues)
 
                 old = False
@@ -244,58 +304,6 @@ class clerk(sim.Component):
 
 
 
-while True:
-    BANDWIDTH = input("Enter bandwidth (Mbps): ") * pow(10,6)
-    if BANDWIDTH > 0:
-        break
-    print "invalid input, try again!"
-
-while True:
-    SPARSEFLOWS = input("Enter number of sparseflows: ")
-    if SPARSEFLOWS > 0:
-       break
-    print "invalid input, try again!"
-
-while True:
-    SPARSESIZE = input("Enter sparseflow packetsize(Byte): ") * 8.0
-    if SPARSESIZE > 0 or SPARSESIZE == -1:
-        break
-    print "invalid input, try again!"
-while True:
-    BULKFLOWS = input("Enter number of bulkflows: ")
-    if BULKFLOWS >= 0:
-        break
-    print "invalid input, try again!"
-
-while True:
-    INTERARRIVALTIME = input("Enter interarrival time(ms): ") * pow(10,-3)
-    if INTERARRIVALTIME > 0 or INTERARRIVALTIME == -1:
-        break
-    print "invalid input, try again!"
-
-while True:
-    INTERARRIVALMULTIPLIER = input("Enter interarrivaltime multiplier ")
-    if INTERARRIVALMULTIPLIER > 0:
-        break
-    print "invalid input, try again!"
-
-while True:
-    RUNTIME = input("Enter simulated runtime: ")
-    if RUNTIME > 0:
-        break
-    print "invalid input, try again!"
-
-while True:
-    DETAILEDOUTPUT = input("Detailed output?: ")
-    if DETAILEDOUTPUT == 0 or DETAILEDOUTPUT == 1:
-        break
-    print "invalid input, try again!"
-
-while True:
-    TRACE = input("Trace?: ")
-    if TRACE == 0 or TRACE == 1:
-        break
-    print "invalid input, try again!"
 
 
 
@@ -312,20 +320,20 @@ def printQueueDelay(detailed = False):
     averageWaitingTime = 0.0
 
     if detailed:
-	print "+----------+--------------+-----------+"
-	print "| Flow id: | Sparseness % | Time (ms) |"
-	print "+----------+--------------+-----------+"
+	print "+----------+--------------+-------------------+"
+	print "| Flow id: | Sparseness % | Time (ms)         |"
+	print "+----------+--------------+-------------------+"
 	
     for x in range(len(passiveQueues)):
         if int(passiveQueues[x].packetCounter) == 0:
-            print "| ", str(passiveQueues[x].qid).ljust(6), "Did not activate".ljust(18), " | "
+            print "| ", str(passiveQueues[x].qid).ljust(6), "Did not activate".ljust(26), " | "
             continue
 
         if detailed:
             print "| ", str(passiveQueues[x].qid).ljust(6), " |",
             print " ", str(round((passiveQueues[x].activateCounter / passiveQueues[x].packetCounter) * 100, 4)).ljust(9), " |", 
-            print " ", str(round((passiveQueues[x].totalQueueDelay / passiveQueues[x].packetCounter) * 1000, 4)).ljust(6), " |"
-            print "+----------+--------------+-----------+"
+            print " ", str(round((passiveQueues[x].totalQueueDelay / passiveQueues[x].packetCounter) * 1000, 4)).ljust(14), " |"
+            print "+----------+--------------+-------------------+"
 
         if passiveQueues[x].qid % 2 == 0:
             averageWaitingTime += passiveQueues[x].totalQueueDelay/passiveQueues[x].packetCounter
@@ -335,8 +343,8 @@ def printQueueDelay(detailed = False):
         if detailed:
             print "| ", str(newQueues[x].qid).ljust(6), " |",
             print " ", str(round((newQueues[x].activateCounter / newQueues[x].packetCounter) * 100, 4)).ljust(9), " |", 
-            print " ", str(round((newQueues[x].totalQueueDelay / newQueues[x].packetCounter) * 1000, 4)).ljust(6), " |"
-            print "+----------+--------------+-----------+"
+            print " ", str(round((newQueues[x].totalQueueDelay / newQueues[x].packetCounter) * 1000, 4)).ljust(14), " |"
+            print "+----------+--------------+-------------------+"
             
 
         if newQueues[x].qid % 2 == 0:
@@ -347,8 +355,8 @@ def printQueueDelay(detailed = False):
         if detailed:
             print "| ", str(oldQueues[x].qid).ljust(6), " |",
             print " ", str(round((oldQueues[x].activateCounter / oldQueues[x].packetCounter) * 100, 4)).ljust(9), " |", 
-            print " ", str(round((oldQueues[x].totalQueueDelay / oldQueues[x].packetCounter) * 1000, 4)).ljust(6), " |"
-            print "+----------+--------------+-----------+"
+            print " ", str(round((oldQueues[x].totalQueueDelay / oldQueues[x].packetCounter) * 1000, 4)).ljust(14), " |"
+            print "+----------+--------------+-------------------+"
             
 	if oldQueues[x].qid % 2 == 0:
             averageWaitingTime += oldQueues[x].totalQueueDelay/oldQueues[x].packetCounter
@@ -374,15 +382,10 @@ else:
 passiveQueues = sim.Queue('passiveQueues')
 
 for _ in range(SPARSEFLOWS + BULKFLOWS):
-        queue().enter(passiveQueues)
+    queue().enter(passiveQueues)
 
-gen = genFlow(waitTime=time)
+gen = flowGenerator(waitTime=time)
         
-for i in range(1,2*BULKFLOWS,2):
-    bulkFlowGenerator(fid=i,interTime=((QUANTUM/BANDWIDTH)-(1.0/BANDWIDTH)),distribution="uniform")
-
-
-
 
 newQueues = sim.Queue('newQueues')
 oldQueues = sim.Queue('oldQueues')
